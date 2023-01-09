670b4c4368977576e86c4006d0d9525f0cbfbf60
Rahulaustralia <rahul090897pandey@gmail.com>
2023-01-09 14:19:54 +0530

    add

--- my-c3-evl/pages/index.tsx
+++ my-c3-evl/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
+        {/* <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
@@ -116,7 +116,7 @@ export default function Home() {
               with&nbsp;Vercel.
             </p>
           </a>
-        </div>
+        </div> */}
       </main>
     </>
   )
